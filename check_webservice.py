1539a2d1114b36309470e3f070575ec5d94569ec
simrat pal singh <simrat.khokhar@gmail.com>
2018-02-21 00:22:29 +0530

    check_webservice.py: add cmdline options

--- check_webservice.py
+++ check_webservice.py
@@ -2,32 +2,66 @@
 
 import httplib
 import sys
+import socket
+import optparse
 
-def CheckWebservice(address, port, resource):
-  #if not resource.startswith('/'):
-  #  resource = '/' + resource
+#proxy = 'http://192.168.56.1:3128'
+#os.environ['http_proxy'] = proxy
+#os.environ['HTTP_PROXY'] = proxy
+#os.environ['https_proxy'] = proxy
+#os.environ['HTTPS_PROXY'] = proxy
 
-  try:
-    conn = httplib.HTTPConnection(address, port)
-    print "[*] http connection created successfully"
-    req = conn.request('GET', resource)
-    print "[=] request for %s successful" % resource
-    response = conn.getresponse()
-    print "response status: %s" % response.status
-  except socket.error, e:
-    print "[=] http connection failed: %s" % e
-    return False
-  finally:
-    conn.close()
-    print "[=] http conn closed successful"
-    
-  if response.status in [200, 301]:
-    return True
-  else:
-    return False
-    
+def WebService(address, port, resource, https_enable, print_enable):
+    #if not resource.startswith('/'):
+    #    resource = '/' + resource
+
+    try:
+        if https_enable is True:
+            conn = httplib.HTTPSConnection(address, port, timeout=20)
+            print "[=] HTTPS connection created successfully"
+        else:
+            conn = httplib.HTTPConnection(address, port, timeout=20)
+            print "[=] HTTP connection created successfully"
+        #conn.set_tunnel('192.168.56.1', 3128)
+        req = conn.request('GET', resource)
+        print "[=] request for: %s" % resource
+        response = conn.getresponse()
+        print "[=] response status: %s: %s" % (response.status, response.reason)
+        if print_enable is True:
+            print "[=] response string: \n%s" % response.read()
+    except socket.error, e:
+        print "[=] HTTP connection failed: %s" % e
+        return False
+    finally:
+        conn.close()
+
+    if response.status in [200, 301]:
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
-  #check = CheckWebservice('192.168.56.1', 3128, 'www.google.co.in')
-  check = CheckWebservice('192.168.56.1', 3128, 'www.ipchicken.com') 
-  print "[=] function returned %s" % check
-  sys.exit( not check)
+    parser = optparse.OptionParser()
+    parser.add_option("-a", "--address", dest = "address", default = "localhost",
+            help = "address for server|proxy",
+            metavar = "ADDRESS")
+    parser.add_option("-p", "--port", dest="port", default=80,
+            help="port of webserver",
+            metavar="PORT")
+    parser.add_option("-r", "--resource", dest="resource", default="/",
+            help="resource to GET",
+            metavar="RESOURCE")
+    parser.add_option("-s", "--ssl", dest="https", default=False, action="store_true", 
+            help="enable HTTPS",
+            metavar="HTTPS")
+    parser.add_option("-e", "--echo", dest="print_resp", default=False, action="store_true",
+            help="echo response to stdout")
+    (options, args) = parser.parse_args()
+
+    #check = WebService('192.168.56.1', 3128, 'http://www.google.co.in')
+    check = WebService(options.address, options.port, options.resource, options.https, options.print_resp)
+    print "[=] return status of WebService: %d" % check
+    sys.exit(not check)
+# -- end --
+
