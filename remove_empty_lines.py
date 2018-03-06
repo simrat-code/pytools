99dd560c4b443315a37bde476acfea2018f5a232
simrat pal singh <simrat.khokhar@gmail.com>
2018-03-06 14:33:16 +0530

    class 'FileWithEmptyLines' and optparse module has been added

--- remove_empty_lines.py
+++ remove_empty_lines.py
@@ -2,25 +2,76 @@
 
 import os
 import sys
+import optparse
 
 
-def remove_empty_lines(f):
-    if not os.path.isfile(f):
-        print "does not exist %s" % f
-        return
-    fh_in = open(f, "r")
-    fh_out = open("without_empty.txt", 'w')
+class FileWithEmptyLines:
+    def __init__(self):
+        self.verbose = False
+        self.infile = "with_empty.txt"
+        self.outfile = "out_noempty.txt"
 
-    for line in fh_in.readlines():
-        if line.rstrip() == "":
-            continue
-        print "LINE: %s" % line,
-        fh_out.writelines(line)
 
-    fh_in.close()
-    fh_out.close()
+    def set_source_file(self, fname):
+        self.infile = fname
+
+
+    def set_dest_file(self, fname):
+        self.outfile = fname
+
+
+    def set_verbose(self, option=True):
+        self.verbose = option
+
+
+    def print_filename(self):
+        print "in file : %s" % self.infile
+        print "out file: %s" % self.outfile
+
+
+    def remove_empty_lines(self):
+        if not os.path.isfile(self.infile):
+            print "does not exist %s" % f
+            return
+        fh_in = open(self.infile, "r")
+        fh_out = open(self.outfile, 'w')
+    
+        for line in fh_in.readlines():
+            if line.rstrip() == "":
+                continue
+            if self.verbose is True:
+                print "LINE: %s" % line,
+            fh_out.writelines(line)
+    
+        fh_in.close()
+        fh_out.close()
 
 
 if __name__ == "__main__":
-    remove_empty_lines("with_empty.txt")
+    parser = optparse.OptionParser()
+    parser.add_option('-v', '--versose', dest="verbose", 
+            default=False,
+            action="store_true",
+            help="echo each printable line",
+            metavar="VERBOSE")
+    parser.add_option('-s', '--source', dest="source",
+            help="source file to read from",
+            metavar="SOURCE")
+    parser.add_option('-o', '--outfile', dest="outfile",
+            help="filtered data written to outfile",
+            metavar="OUTFILE")
+    (options, args) = parser.parse_args()
+
+    fm = FileWithEmptyLines()
+    if options.source:
+        fm.set_source_file(options.source)
+    if options.outfile :
+        fm.set_dest_file(options.outfile)
+    if options.verbose:
+        fm.set_verbose()
+
+    fm.print_filename()
+    fm.remove_empty_lines()
+
+# -- end --
 
