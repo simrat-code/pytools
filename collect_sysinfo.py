ac35342ade26ffa1c03ced59d2610e323b9ac9ef
Simrat Singh <simrat.khokhar@gmail.com>
2021-06-16 15:36:29 +0530

    code modified

--- collect_sysinfo.py
+++ collect_sysinfo.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 import subprocess
-import types
-import time
 
 cmd_date = ["date", "+%a %d-%b-%Y %H:%M"]
 cmd_kernel = ["uname", "-r"]
@@ -23,45 +21,46 @@ cmd_list = [
         ]
 
 def show_command(cmd):
-    if type(cmd) != list:
-        raise TypeError
+    if not isinstance(cmd, list): return False
     print("CMD: \t", end='')
     print(*cmd, sep=" ")
+    return True
 
 
 def execute_command(cmd):
-    try:
-        show_command(cmd)
-    except TypeError:
-        print("[X] expect List, but provided {}".format(type(cmd)))
+    print("=" * 60)
+    if not show_command(cmd):
+        print("[x] expect List, but provided {}".format(type(cmd)))
         return
-
-    p = subprocess.Popen(cmd,
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE)
+    out = err = None
     try:
+        p = subprocess.Popen(cmd,
+                stdout = subprocess.PIPE,
+                stderr = subprocess.PIPE)
         out, err = p.communicate(timeout = 20)
-    except TimeoutExpired:
+    except subprocess.TimeoutExpired:
+        # https://docs.python.org/3/library/subprocess.html
+        # The child process is not killed if the timeout expires, 
+        # so in order to cleanup properly a well-behaved application should 
+        # kill the child process and finish communication
         print("[=] timeout occured")
         p.kill()
         out, err = p.communicate()
-
-    if out:
-        print("OUTPUT:\n{}".format(out.decode('utf-8')))
-    else:
-        print("ERROR:\n{}".format(err.decode('utf-8')))
-
-
-def separator():
-    print("=" * 60)
+    except FileNotFoundError as e:
+        err = "exception: {}".format(e).encode()
+    except:
+        err = "[x] unhandled exception occurs".encode()
+    finally:
+        if out: print("OUTPUT:\n{}".format(out.decode('utf-8')))
+        else: print("ERROR: {}".format(err.decode('utf-8')))
 
 
 if __name__ == "__main__":
-    for c in cmd_list:
-        try:
-            separator()
-            execute_command(c)
-        except FileNotFoundError as e:
-            print("[X] Exception: {}".format(e))
+    list(map(execute_command, cmd_list))
+    # for c in cmd_list:
+    #     try:
+    #         execute_command(c)
+    #     except FileNotFoundError as e:
+    #         print("[X] Exception: {}".format(e))
 
 
